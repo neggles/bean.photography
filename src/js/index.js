125511aef323f14b019804979fb193598c2dafc2
Andi Powers-Holmes <aholmes@omnom.net>
2025-10-26 18:13:46 +1100

    ok this works now

--- src/js/index.js
+++ src/js/index.js
@@ -13,66 +13,51 @@ function randInt(min, max) {
     return Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min) + 1)) + min;
 }
 
-function pickRandomBean() {
-    if (randInt(0, 69) == 69) return special_bean;
-    return beans[randInt(0, beans.length - 1)];
+function pickBeanSrc() {
+    if (randInt(0, 69) === 69) {
+        console.info("You found the special bean! 🎉");
+        return special_bean;
+    } else {
+        return beans[randInt(0, beans.length - 1)];
+    }
 }
 
-function getBeanImage() {
-    let img = document.createElement("img");
-    img.src = pickRandomBean();
+/**
+ * Load a random bean image and invoke a callback when it finishes loading.
+ *
+ * @param {(img: HTMLImageElement) => void} cb - Callback invoked with the loaded HTML image element.
+ * @returns {HTMLImageElement} The created image element whose src is set.
+ */
+function getBeanImage(cb) {
+    const img = document.createElement("img");
+    img.addEventListener("load", () => cb(img), { once: true });
+    img.addEventListener("error", (e) => {
+        console.error("Failed to load bean image:", img.src, e);
+    });
+    img.style.display = "none";
     img.style.position = "absolute";
-    img.style.userSelect = "none";
-    img.style.pointerEvents = "none";
+    img.src = pickBeanSrc();
     return img;
 }
 
-function move(image, x, y) {
-    image.style.left = x;
-    image.style.top = y;
-}
-
-function getImageSizePx(image) {
-    // get the dimensions from the width and height attributes
-    let width = image.getAttribute("naturalWidth") || image.getAttribute("width");
-    let height = image.getAttribute("naturalHeight") || image.getAttribute("height");
-
-    if (width != null && height != null) {
-        return { w: parseInt(width), h: parseInt(height) };
-    }
-    // get the dimensions from the viewBox attribute
-    let viewBox = image.getAttribute("viewBox");
-    if (viewBox != null) {
-        let dimensions = viewBox.split(" ");
-        if (dimensions.length == 4) {
-            return { w: parseInt(dimensions[2]), h: parseInt(dimensions[3]) };
-        }
-    }
-    // return default dimensions if none are found
-    return { w: 512, h: 512 };
-}
-
-function checkRow(data, width, y) {
-    for (let x = 0, index = 4 * y * width; x < width; ++x, index += 4) {
-        if (data[index + 3] !== 0) return false;
-    }
-    return true;
-}
-
-function checkColumn(data, width, x, top, bottom) {
-    const stride = 4 * width;
-    for (let y = top, index = top * stride + 4 * x; y <= bottom; ++y, index += stride) {
-        if (data[index + 3] !== 0) return false;
-    }
-    return true;
-}
-
-function getBoundingBox(canvas) {
-    const ctx = canvas.getContext("2d");
-    const width = canvas.width;
-    const height = canvas.height;
+function getBoundingBox(ctx, width, height) {
     const imageData = ctx.getImageData(0, 0, width, height);
     const data = imageData.data;
+
+    function checkRow(data, width, y) {
+        for (let x = 0, index = 4 * y * width; x < width; ++x, index += 4) {
+            if (data[index + 3] !== 0) return false;
+        }
+        return true;
+    }
+
+    function checkColumn(data, width, x, top, bottom) {
+        const stride = 4 * width;
+        for (let y = top, index = top * stride + 4 * x; y <= bottom; ++y, index += stride) {
+            if (data[index + 3] !== 0) return false;
+        }
+        return true;
+    }
 
     let left = 0;
     let top = 0;
@@ -98,22 +83,35 @@ function getBoundingBox(canvas) {
     };
 }
 
-// const _internal_canvas = new OffscreenCanvas(512, 512);
-
-// function getImageBbox(image) {
-//     const ctx = _internal_canvas.getContext("2d");
-//     if (!ctx) {
-//         console.error("OffscreenCanvas context not found");
-//         return null;
-//     }
-//     ctx.drawImage(image, 0, 0);
-//     return getBoundingBox(_internal_canvas);
-// }
+function getImageBbox(image) {
+    const canvas = document.createElement("canvas");
+    canvas.width = image.naturalWidth || image.width;
+    canvas.height = image.naturalHeight || image.height;
+    const ctx = canvas.getContext("2d");
+    if (!ctx) {
+        console.error("Canvas context not found");
+        return null;
+    }
+    ctx.drawImage(image, 0, 0);
+    return getBoundingBox(ctx, canvas.width, canvas.height);
+}
 
 // constants
 const params = new URLSearchParams(window.location.search);
-const bean = getBeanImage();
-const speed = params.get("speed") || 1.0;
+let beanBbox = null;
+let speed = 1.0;
+
+/** @type {HTMLImageElement} **/
+const bean = getBeanImage((img) => {
+    beanBbox = getImageBbox(img);
+    if (beanBbox) {
+        console.log("Bean bounding box:", beanBbox);
+    } else {
+        console.log("Could not determine bean bounding box");
+    }
+    // speed in pixels per frame
+    pageEffects.speed = parseFloat(params.get("speed")) || 1.0;
+});
 
 // variables
 let direction = { x: randInt(0, 1) === 0 ? -1 : 1, y: randInt(0, 1) === 0 ? -1 : 1 };
@@ -129,21 +127,24 @@ class PageEffects {
     dvd;
     dvdframe;
     parent;
+    speed;
+    events = {};
 
     constructor(parent) {
-        this.parent = parent;
-        if (typeof parent === "string") this.parent = document.querySelector(parent);
+        /** @type {HTMLElement} **/
+        this.parent = typeof parent === "string" ? document.querySelector(parent) : parent;
         if (!this.parent) throw new Error("Parent not found");
 
-        this.events = {
-            resize: this.onResize.bind(this),
-        };
+        // event handlers
+        this.events = { resize: this.onResize.bind(this) };
 
-        // ensure initial position is within the window bounds so we can catch the bounding box correctly
-        this.xpos = randInt(0, window.innerWidth - 512 - 1);
-        this.ypos = randInt(0, window.innerHeight - 512 - 1);
+        // ensure initial position is within the window bounds
+        this.xpos = randInt(1, window.innerWidth - 1);
+        this.ypos = randInt(1, window.innerHeight - 1);
+
         this.dvd = {};
         this.dvdframe = undefined;
+        this.speed = 1.0;
 
         window.addEventListener("resize", this.events.resize, false);
 
@@ -152,10 +153,8 @@ class PageEffects {
     }
 
     onResize(e) {
-        this.rect = {
-            width: window.innerWidth,
-            height: window.innerHeight,
-        };
+        this.rect = { width: window.innerWidth, height: window.innerHeight };
+        this.initDVD(); // reinitialize DVD effect on resize
     }
 
     // DVD Screensaver Effect
@@ -165,20 +164,22 @@ class PageEffects {
             this.dvdframe = undefined;
         }
 
+        /** @type {HTMLCanvasElement} **/
         const canvas = document.getElementById("dvd");
         if (!canvas) {
             console.error("DVD canvas not found");
             return;
         }
+        /** @type {CanvasRenderingContext2D} **/
         const ctx = canvas.getContext("2d", { willReadFrequently: true });
         if (!ctx) {
             console.error("DVD canvas context not found");
             return;
         }
 
-        canvas.width = this.rect.width * 0.8;
-        canvas.height = this.rect.height * 0.8;
-        this.dvd = { canvas: canvas, ctx: ctx, bbox: null };
+        canvas.width = this.rect.width;
+        canvas.height = this.rect.height;
+        this.dvd = { canvas: canvas, ctx: ctx, isize: null, bbox: null };
 
         const animate = () => {
             this.renderDVD();
@@ -188,47 +189,39 @@ class PageEffects {
     }
 
     renderDVD() {
-        const canvas = this.dvd.canvas;
-        if (!canvas) return;
-        const ctx = this.dvd.ctx;
+        // early out if bean image is not loaded yet
 
-        // move the logo to the current X and Y coords
+        if (!bean) return;
+
+        /** @type {HTMLCanvasElement} **/
+        const canvas = this.dvd.canvas;
+        /** @type {CanvasRenderingContext2D} **/
+        const ctx = this.dvd.ctx;
+        if (!ctx) return;
+
+        // clear canvas and draw bean at current position
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.drawImage(bean, this.xpos, this.ypos);
 
-        // get the bounding box of the bean image
-        if (!this.dvd.bbox) {
-            let bbox = getBoundingBox(canvas);
-            if (bbox) this.dvd.bbox ??= bbox;
-        }
+        if (beanBbox) {
+            const margin = 32;
 
-        if (this.dvd.bbox != null) {
-            // ok so we're storing top-left corner of image object but we want our collision check to use 10px
-            // in from the actual non-transparent image bbox
-            const offset = 10;
-
-            const leftEdge = this.xpos + -offset;
-            const rightEdge = this.xpos + (this.dvd.bbox.left + this.dvd.bbox.width) + offset;
-            const topEdge = this.ypos + this.dvd.bbox.top - offset;
-            const bottomEdge = this.ypos + (this.dvd.bbox.top + this.dvd.bbox.height) + offset;
+            const leftEdge = this.xpos + beanBbox.left + margin;
+            const rightEdge = this.xpos + beanBbox.right - margin;
+            const topEdge = this.ypos + beanBbox.top + margin;
+            const bottomEdge = this.ypos + beanBbox.bottom - margin;
 
             // check if logo is bouncing on the left/right side
-            if (leftEdge <= 1) {
-                changeDirection("x", 1);
-            } else if (rightEdge + 1 >= this.rect.width) {
-                changeDirection("x", -1);
-            }
+            if (leftEdge <= 1) changeDirection("x", 1);
+            else if (rightEdge + 1 >= this.rect.width) changeDirection("x", -1);
 
             // check if logo is bouncing on the top/bottom side
-            if (topEdge <= 1) {
-                changeDirection("y", 1);
-            } else if (bottomEdge + 1 >= this.rect.height) {
-                changeDirection("y", -1);
-            }
+            if (topEdge <= 1) changeDirection("y", 1);
+            else if (bottomEdge + 1 >= this.rect.height) changeDirection("y", -1);
 
             // update position, we'll write the image in its new position in the next frame
-            this.xpos += speed * direction.x;
-            this.ypos += speed * direction.y;
+            this.xpos += this.speed * direction.x;
+            this.ypos += this.speed * direction.y;
         }
     }
 }
